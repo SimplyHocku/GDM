b50cc4b81d4f0ccb588b5855866f413cd77e8828
hocku <gajo216@gmail.com>
2024-09-25 19:46:51 +0300

    msg

---
+++ src/main/java/org/example/CreateExcelFile.java
@@ -0,0 +1,98 @@
+package org.example;
+
+import com.codeborne.selenide.ElementsCollection;
+import com.codeborne.selenide.SelenideElement;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFCell;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.List;
+
+public class CreateExcelFile {
+
+    public static void main(String[] args) {
+        JiraPage page = new JiraPage();
+        ElementsCollection tasks = page.start().getRowContent();
+        if (tasks.isEmpty()) {
+            return;
+        }
+
+        List<SelenideElement> copiedElements = tasks.stream()
+                .toList();
+
+
+        XSSFWorkbook workbook = new XSSFWorkbook();
+        XSSFSheet sheet = workbook.createSheet("TestCases");
+
+        XSSFRow headerRow = sheet.createRow(0);
+        String[] headers = {
+                "№", " ", "ТК", "Название", "Количество тестов", "Количество шагов",
+                "Ревью ТК", "Приоритет", "Статус задачи", "Статус проверки", "Тестировщик"
+        };
+
+        for (int i = 0; i < headers.length; i++) {
+            XSSFCell cell = headerRow.createCell(i);
+            cell.setCellValue(headers[i]);
+        }
+        int rowNumber = 1;
+        int taskNumber = 0;
+
+        for (SelenideElement copiedElement : copiedElements) {
+            taskNumber += 1;
+            String issueType = copiedElement.$x(".//td[@class = 'issuetype']").$x(".//img").getAttribute("alt");
+            String issueKey = copiedElement.$x(".//td[@class = 'issuekey']").$x(".//a").getText();
+            String issueKeyLink = "https://jira.mos.social/browse/" + issueKey;
+            String summary = copiedElement.$x(".//td[@class = 'summary']").$x(".//a[@class = 'issue-link']").getText();
+            String priority = copiedElement.$x(".//td[@class = 'priority']").$x(".//img").getAttribute("alt");
+            String status = copiedElement.$x(".//td[@class = 'status']").$x(".//span").getText();
+
+            String[] rowData = {Integer.toString(taskNumber), issueType, issueKeyLink, summary, priority, status};
+            Integer[] rowNumbers = {0, 1, 2, 3, 7, 8};
+            XSSFRow row = sheet.createRow(rowNumber);
+            for (int cellNumber = 0; cellNumber < 6; cellNumber++) {
+                XSSFCell cell = row.createCell(rowNumbers[cellNumber]);
+                cell.setCellValue(rowData[cellNumber]);
+            }
+
+            ElementsCollection taskCases = page.getTestCases(issueKey);
+
+
+            if (!taskCases.isEmpty()) {
+                for (int tk = 0; tk < taskCases.size(); tk++) {
+                    rowNumber += 1;
+                    row = sheet.createRow(rowNumber);
+                    issueType = taskCases.get(tk).$x(".//td[@class = 'issuetype']").$x(".//img").getAttribute("alt");
+                    issueKey = "https://jira.mos.social/browse/" + taskCases.get(tk).$x(".//td[@class = 'issuekey']").$x(".//a").getText();
+                    summary = taskCases.get(tk).$x(".//td[@class = 'summary']").$x(".//a[@class = 'issue-link']").getText();
+                    priority = taskCases.get(tk).$x(".//td[@class = 'priority']").$x(".//img").getAttribute("alt");
+                    status = taskCases.get(tk).$x(".//td[@class = 'status']").$x(".//span").getText();
+
+                    rowData = new String[]{issueType, issueKey, summary, status, priority};
+                    rowNumbers = new Integer[]{1, 2, 3, 6, 7};
+
+                    for (int cellNumber = 0; cellNumber < 5; cellNumber++) {
+                        XSSFCell cell = row.createCell(rowNumbers[cellNumber]);
+                        cell.setCellValue(rowData[cellNumber]);
+                    }
+                }
+            }
+            page.getDriver().switchTo().window(page.getTab(0));
+            rowNumber += 1;
+        }
+
+        try (FileOutputStream fileOut = new FileOutputStream("гдшка.xlsx")) {
+            workbook.write(fileOut);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                workbook.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
---
+++ src/main/java/org/example/JiraPage.java
@@ -0,0 +1,125 @@
+package org.example;
+
+import com.codeborne.selenide.*;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.Set;
+
+import static com.codeborne.selenide.Selenide.*;
+
+public class JiraPage {
+
+
+    private final SelenideElement loginInput = $x("//input[@id = 'login-form-username']");
+    private final SelenideElement passwordInput = $x("//input[@id = 'login-form-password']");
+    private final SelenideElement btnSubmitLogin = $x("//button[@id = 'login-form-submit']");
+
+    private String project = "";
+    private String fixVersion = "";
+    private String login = "";
+    private String password = "";
+    private String status = "";
+    String urlSearch;
+
+    private WebDriver driver;
+    static String firstTab, secondTab;
+
+    {
+        this.readData();
+        String loginUrl = "https://jira.mos.social/login.jsp?os_destination=%2Fsecure%2FMyJiraHome.jspa";
+        Selenide.open(loginUrl);
+        this.driver = WebDriverRunner.getWebDriver();
+
+        $(".aui-page-panel").shouldBe(Condition.visible);
+
+
+        this.loginInput.setValue(login);
+        this.passwordInput.setValue(password);
+        this.btnSubmitLogin.click();
+
+        firstTab = driver.getWindowHandle();
+        ((JavascriptExecutor) driver).executeScript("window.open()");
+        Set<String> windowHandles = driver.getWindowHandles();
+        secondTab = windowHandles.stream()
+                .filter(handle -> !handle.equals(firstTab))
+                .findFirst()
+                .orElse(null);
+
+        if (secondTab != null) {
+            driver.switchTo().window(secondTab);
+            Selenide.open("https://jira.mos.social/issues");
+        }
+        driver.switchTo().window(firstTab);
+
+    }
+
+    private void readData() {
+        String filePath = "/home/hocku/PFLPS.txt";
+        try {
+            List<String> lines = Files.readAllLines(Paths.get(filePath));
+
+            for (int row = 0; row < lines.size(); row++) {
+                if (row == 0) {
+                    project = lines.get(row);
+                } else if (row == 1) {
+                    fixVersion = lines.get(row);
+                } else if (row == 2) {
+                    login = lines.get(row);
+                } else if (row == 3) {
+                    password = lines.get(row);
+                } else if (row == 4) {
+                    status = lines.get(row);
+                    if (lines.get(row).split(",").length > 1) {
+                        String[] statuses = lines.get(row).split(",");
+                        status = String.join("%2C%20", statuses);
+                    }
+                    urlSearch = "https://jira.mos.social/issues/?jql=project%%20%%3D%%20%s%%20AND%%20fixVersion%%20%%3D%%20%s%%20and%%20status%%20in%%20(%s)%%20%%20%%20ORDER%%20BY%%20priority%%20DESC%%2C%%20key%%20ASC".formatted(project, fixVersion, status);
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public JiraPage start() {
+        Selenide.open(urlSearch);
+        return this;
+    }
+
+    ElementsCollection getRowContent() {
+        return $$x("//tr[contains(@id, 'issue')]");
+    }
+
+    ElementsCollection getTestCases(String taskName) {
+        this.driver.switchTo().window(secondTab);
+        Selenide.open("https://jira.mos.social/issues/?jql=issueFunction%%20in%%20linkedIssuesOf(%%22key%%20%%3D%%20%s%%22%%2C%%20%%22is%%20tested%%20by%%22)".formatted(taskName));
+        ElementsCollection testCases = $$x("//tr[contains(@id, 'issue')]");
+        if (!testCases.isEmpty()) {
+            return testCases;
+        }
+        Selenide.open("https://jira.mos.social/issues/?jql=issueFunction%%20in%%20linkedIssuesOf(%%22key%%20%%3D%%20%s%%22%%2C%%20%%22tested%%20by%%22)".formatted(taskName));
+        if (!testCases.isEmpty()) {
+            return testCases;
+        }
+        return testCases;
+    }
+
+    public WebDriver getDriver() {
+        return this.driver;
+    }
+
+    public String getTab(int num) {
+        if (num == 0) {
+            return firstTab;
+        } else if (num == 1) {
+            return secondTab;
+        }
+        return "";
+    }
+}
